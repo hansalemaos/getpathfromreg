46d931006a5d7c9967914ecfea526e507d287e59
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2023-03-18 22:27:02 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,3 +22,13 @@ def get_tesseract_exe():
     pa = f'{os.sep}'.join(di[tesserkey][tesserf].split(os.sep)[:-1])
     pa = os.path.normpath(os.path.join(pa,'tesseract.exe'))
     return pa
+
+def get_hd_player_bluestacks():
+    di = search_values(
+        mainkeys=r"HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks_nxt", subkeys="InstallDir"
+    )
+    bstconfigpath = di[r"HKEY_LOCAL_MACHINE\SOFTWARE\BlueStacks_nxt"]["InstallDir"]
+    bstconfigpath = os.path.normpath(os.path.join(bstconfigpath, "HD-Player.exe"))
+    return bstconfigpath
+
+
